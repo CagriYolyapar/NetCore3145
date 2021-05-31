872d68122be638cd68f010439745957a6a9872f5
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-05-31 14:03:06 +0300

    Category Crud AddCategory ve CategoryList yazıldı

---
+++ CoreMVCIntro/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreMVCIntro.Models.Context;
+using CoreMVCIntro.Models.Entities;
+using CoreMVCIntro.VMClasses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCIntro.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+
+        //SOLID 
+
+        //Dependency Inversion Prensibini uygulamak icin Dependency Injection tasarım paternini kullanırız...Bu patern istedigimiz şekilde istedigimiz sorumlulugun hemen o an icin enjekte edilmesini sağlayan bir tasarım paternidir... Dependency Injection en rahat Interface'ler ile kullanılır... (Böylece istedigimiz an sorumluluğu degiştirebiliriz)...İstisnai olarak DbContext yapısı SingletonPattern calıstıgından dolayı normal şartlar altında Interface üzerinden kullanılmadan DI'a burada enjekte edilmiştir...
+        MyContext _db;
+        public CategoryController(MyContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            CategoryVM cvm = new CategoryVM
+            {
+                Categories = _db.Categories.ToList()
+            };
+            return View(cvm);
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
--- CoreMVCIntro/Startup.cs
+++ CoreMVCIntro/Startup.cs
@@ -85,7 +85,7 @@ namespace CoreMVCIntro
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Category}/{action=Index}/{id?}");
             });
         }
     }
